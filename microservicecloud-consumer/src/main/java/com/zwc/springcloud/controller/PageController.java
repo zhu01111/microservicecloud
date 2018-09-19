701cba978b577b8e965571c65c302dcf45a426f0
朱伟超 <wei.0111@qq.com>
2018-09-19 17:06:47 +0800

    commit

---
+++ microservicecloud-consumer/src/main/java/com/zwc/springcloud/controller/PageController.java
@@ -0,0 +1,30 @@
+/**
+ * FileName:         PageController.java
+ * @author:          zhuWeichao
+ * @version            V1.0
+ * Createdate:      2018年9月19日     下午5:03:03
+ * Copyright:        Copyright(C) 2018
+ * Company           CY.
+ * All rights Reserved, Designed By zhuWeichao
+
+ * Modification  History:
+ * Date         Author        Version        Discription
+ * ---------------------------------------------------------------------------
+ * 2018年9月19日     zhuWeichao       1.0             1.0
+
+ * Why & What is modified:
+
+ */
+package com.zwc.springcloud.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @ClassName:       PageController
+ * @author:          zhuWeichao
+ * @date:            2018年9月19日        下午5:03:03
+ */
+@Controller
+public class PageController {
+	
+}
